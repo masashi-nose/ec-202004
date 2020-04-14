0491445a18b43286f92b0b8e78f2298e21512836
nosemasashi <ma.nose9413@gmail.com>
2020-04-14 22:47:31 +0900

    ItemRepositoryにloadメソッドを追加しました。

--- src/main/java/com/example/demo/repository/ItemRepository.java
+++ src/main/java/com/example/demo/repository/ItemRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.domain.Item;
@@ -46,6 +48,20 @@ public class ItemRepository {
 		String sql = "SELECT id, name, price_m, price_l, image_path, description, deleted FROM items ORDER BY price_m";
 		List<Item> itemList = template.query(sql, ITEM_ROW_MAPPER);
 		return itemList;
+	}
+
+	/**
+	 * idを用いて商品を１件検索します.
+	 * 
+	 * @param id 商品ID
+	 * @return　商品情報
+	 */
+	public Item load(Integer id) {
+		String sql = "SELECT id, name, price_m, price_l, image_path, description, deleted FROM items WHERE id = :id";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+		Item item = template.queryForObject(sql, param, ITEM_ROW_MAPPER);
+		return item;
+
 	}
 
 }
