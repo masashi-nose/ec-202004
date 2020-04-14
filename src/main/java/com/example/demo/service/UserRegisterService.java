bfce3e85f63e81e3386bda4a0924abe19303079b
nosemasashi <ma.nose9413@gmail.com>
2020-04-14 12:17:50 +0900

    UserRegisterserviceを作成ました。

---
+++ src/main/java/com/example/demo/service/UserRegisterService.java
@@ -0,0 +1,33 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.domain.User;
+import com.example.demo.repository.UserRepository;
+
+/**
+ * ユーザー登録の業務処理を行うサービスクラス.
+ * 
+ * @author masashi.nose
+ *
+ */
+@Service
+@Transactional
+public class UserRegisterService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	/**
+	 * ユーザー登録を行います. （ユーザー情報をusersテーブルにインサートします）
+	 * 
+	 * @param user ユーザー情報
+	 * @return IDが自動採番されたUserオブジェクト
+	 */
+	public User register(User user) {
+		return userRepository.insertUser(user);
+
+	}
+}
