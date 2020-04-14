24a75bdb66b1b6faad39fd6dcba69f46923faeab
nosemasashi <ma.nose9413@gmail.com>
2020-04-14 11:51:22 +0900

    first commit

---
+++ src/main/java/com/example/demo/Ec202004Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec202004Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec202004Application.class, args);
+	}
+
+}
