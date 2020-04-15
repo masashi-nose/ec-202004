da63a013bd21035f4166a8f75e108946d99bcc10
nosemasashi <ma.nose9413@gmail.com>
2020-04-15 09:54:48 +0900

    ShowItemDetailServiceにメソッドを追加しました。

--- src/main/java/com/example/demo/service/ShowItemDetailService.java
+++ src/main/java/com/example/demo/service/ShowItemDetailService.java
@@ -1,11 +1,15 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.domain.Item;
+import com.example.demo.domain.Topping;
 import com.example.demo.repository.ItemRepository;
+import com.example.demo.repository.ToppingRepository;
 
 /**
  * 商品詳細表示をする業務処理を行うサービスクラス.
@@ -20,6 +24,9 @@ public class ShowItemDetailService {
 	@Autowired
 	private ItemRepository itemRepository;
 
+	@Autowired
+	private ToppingRepository toppingRepository;
+
 	/**
 	 * 
 	 * IDを用いて商品を１件検索します.
@@ -30,5 +37,15 @@ public class ShowItemDetailService {
 	public Item showDetail(Integer id) {
 		return itemRepository.load(id);
 
+	}
+
+	/**
+	 * 
+	 * toppingsテーブルからトッピングを全件取得し表示します.
+	 * 
+	 * @return トッピング全件情報
+	 */
+	public List<Topping> showToppingList() {
+		return toppingRepository.findAll();
 	}
 }
