c36f6cf8b6c5f0822ed4194aff07a563561cde11
nosemasashi <ma.nose9413@gmail.com>
2020-04-15 09:57:54 +0900

    ShowItemDetailControllerを修正しました。

--- src/main/java/com/example/demo/controller/ShowItemDetailController.java
+++ src/main/java/com/example/demo/controller/ShowItemDetailController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.domain.Item;
+import com.example.demo.domain.Topping;
 import com.example.demo.service.ShowItemDetailService;
 
 /**
@@ -31,7 +34,10 @@ public class ShowItemDetailController {
 	@RequestMapping("/showDetail")
 	public String showDetail(Integer id, Model model) {
 		Item item = showItemDetailService.showDetail(id);
+		List<Topping> toppingList = showItemDetailService.showToppingList();
 
+		item.setToppingList(toppingList);
+		
 		model.addAttribute("item", item);
 
 		return "item_detail";
