0d1defdb8e28aa962186686e35324b6ebf69e54c
nosemasashi <ma.nose9413@gmail.com>
2020-04-16 17:34:55 +0900

    ShoppingCartControllerを作成しました。

--- src/main/java/com/example/demo/controller/ShoppingCartController.java
+++ src/main/java/com/example/demo/controller/ShoppingCartController.java
@@ -1,9 +1,16 @@
 package com.example.demo.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.domain.LoginUser;
+import com.example.demo.form.ShoppingCartForm;
+import com.example.demo.service.OrderService;
 import com.example.demo.service.ShoppingCartService;
 
 /**
@@ -14,12 +21,39 @@ import com.example.demo.service.ShoppingCartService;
  *
  */
 @Controller
-@RequestMapping("")
+@RequestMapping("/cart")
 public class ShoppingCartController {
-	
+
 	@Autowired
 	private ShoppingCartService shoppingCartService;
-	
-	
+
+	@Autowired
+	private OrderService orderService;
+
+	@Autowired
+	private HttpSession session;
+
+	@ModelAttribute
+	public ShoppingCartForm setUpForm() {
+		return new ShoppingCartForm();
+	}
+
+	@RequestMapping("/insert")
+	public String addShoppingCart(ShoppingCartForm form, @AuthenticationPrincipal LoginUser loginUser) {
+		Integer userId = (Integer) session.getAttribute("userId");
+
+		if (userId == null) {
+			session.setAttribute("userId", session.getId());
+
+		}
+
+		if (userId != null) {
+			userId = loginUser.getUser().getId();
+
+		}
+
+		shoppingCartService.insert(form, userId);
+		return "redirect:/";
+	}
 
 }
