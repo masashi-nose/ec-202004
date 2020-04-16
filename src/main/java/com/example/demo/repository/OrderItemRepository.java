7f79a100b60996df0be227aa43dfd0d577605163
nosemasashi <ma.nose9413@gmail.com>
2020-04-16 11:13:45 +0900

    OrderItemRepositoryを作成しました。

---
+++ src/main/java/com/example/demo/repository/OrderItemRepository.java
@@ -0,0 +1,54 @@
+package com.example.demo.repository;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.domain.OrderItem;
+
+/**
+ * order_itemsテーブルを操作するリポジトリ.
+ * 
+ * @author masashi.nose
+ *
+ */
+@Repository
+public class OrderItemRepository {
+
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+
+	private SimpleJdbcInsert insert;
+
+	@PostConstruct
+	public void init() {
+		SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert((JdbcTemplate) template.getJdbcOperations());
+		SimpleJdbcInsert withTableName = simpleJdbcInsert.withTableName("order_items");
+		insert = withTableName.usingGeneratedKeyColumns("id");
+
+	}
+
+	/**
+	 * ID自動採番型のインサートメソッド
+	 * 
+	 * @param orderItem
+	 * @return
+	 */
+	public OrderItem insert(OrderItem orderItem) {
+		SqlParameterSource param = new BeanPropertySqlParameterSource(orderItem);
+
+		if (orderItem.getId() == null) {
+			Number key = insert.executeAndReturnKey(param);
+			orderItem.setId(key.intValue());
+		}
+
+		return orderItem;
+	}
+
+}
